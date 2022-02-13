213b648052570e72b2c2da8119afbbe4bdbcb067
Taysser <tgherfal@outlook.com>
2022-02-12 20:27:18 -0800

    test

--- athan.py
+++ athan.py
@@ -98,26 +98,22 @@ while True:
             ntime=ptime[0]
             location=0
         else:
-            if current_time <= ptime[1]:
-                ntime=ptime[1]
-                location=1
+            if current_time <= ptime[2]:
+                ntime=ptime[2]
+                location=2
             else:
-                if current_time <= ptime[2]:
-                    ntime=ptime[2]
-                    location=2
+                if current_time <= ptime[3]:
+                    ntime=ptime[3]
+                    location=3
                 else:
-                    if current_time <= ptime[3]:
-                        ntime=ptime[3]
-                        location=3
+                    if current_time <= ptime[4]:
+                        ntime=ptime[4]
+                        location=4
                     else:
-                        if current_time <= ptime[4]:
-                            ntime=ptime[4]
-                            location=4
-                        else:
-                            ntime=ptime[0]
-                            location=0
+                        ntime=ptime[1]
+                        location=1
         display.updateDisplay(ntime, location)
-        print(str(ptime[0]) + " -- " + str(current_time) + "--" + str(current_time > ptime[1]))
+        print(str(ptime[2]) + " -- " + str(current_time) + "--" + str(current_time > ptime[2]))
         print("------------------------------------------------------------")
         print()
         # startup indicator
