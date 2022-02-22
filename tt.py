8240f4d2c02cc5aa3197cf5a7548f9a576553980
Taysser <tgherfal@outlook.com>
2022-02-21 23:22:05 -0800

    TKinter Testing

---
+++ tt.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+root = tk.Tk()
+
+# window title
+root.title("Athan")
+
+# window size
+root.geometry('600x400')
+
+# place a label on the root window
+message = tk.Label(root, text="Hello, World!")
+message.pack()
+
+# label with a specific font
+label = ttk.Label(
+    root,
+    text='Fajer',
+    font=("Helvetica", 100))
+
+label.pack(ipadx=10, ipady=10)
+
+# keep the window displaying
+root.mainloop()
