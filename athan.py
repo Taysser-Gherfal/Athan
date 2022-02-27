625b565a3966c39f9ac8e23fec7dd529f55bb58b
Taysser <tgherfal@outlook.com>
2022-02-27 01:17:12 -0800

    investigation 4

--- athan.py
+++ athan.py
@@ -146,7 +146,7 @@ while True:
             if t > time.strptime(ptime[0], '%I:%M %p'):
                 ntime=ptime[1]
                 location=1
-                print(str(time.strptime(ptime[0], '%I:%M %p, %Y')))
+                print(time.strptime(ptime[0], '%I:%M %p, %Y'))
             else:
                 ntime=ptime[0]
                 location=0
